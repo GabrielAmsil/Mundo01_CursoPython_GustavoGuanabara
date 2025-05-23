d34c4a9828c7256b071f60c746019acac7bff290
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:50:30 -0300

    Delete Ex012.py

--- Ex012.py
+++
@@ -1,4 +0,0 @@
-preço = float(input('Digite o preço do produto: R$'))
-desconto = 20
-preço_final = preço - (preço * desconto / 100)
-print('O preço do produto com desconto de {}% é R${:.2f}'.format(desconto, preço_final))
