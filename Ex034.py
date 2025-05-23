def8b3f3435dd08a7e29571c9835e8de47c96dfa
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:53:31 -0300

    Delete Ex034.py

--- Ex034.py
+++
@@ -1,8 +0,0 @@
-sal = float(input('Qual o salário do funcionário? R$'))
-if sal <= 1250:
-    aumento = sal * 0.15
-if sal > 1250:
-    aumento = sal * 0.10
-    novo_sal = sal + aumento
-novo_sal = sal + aumento
-print(f'O novo salário do funcionário é R${novo_sal}')
