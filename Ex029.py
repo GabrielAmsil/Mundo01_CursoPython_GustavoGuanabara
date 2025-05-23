b2819de704b9a5b59e6063c321b6a77bf378ff54
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:52:57 -0300

    Delete Ex029.py

--- Ex029.py
+++
@@ -1,9 +0,0 @@
-carro = float(input('Qual a velocidade do carro? '))
-if carro > 80:
-    print('Você ultrapassou a velocidade permitida de 80km/h')
-multa = (carro - 80) * 7
-if multa > 0:
-    print(f'Você pagará uma multa de R${multa:.2f}')
-else:
-    print('Você está dentro do limite de velocidade')
-        
