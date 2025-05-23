129eeb1e63dce4d05b10234fe0dabbf88dd1a946
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:50:38 -0300

    Delete Ex013.py

--- Ex013.py
+++
@@ -1,4 +0,0 @@
-salário = float(input('Qual o slário do funcionário? R$'))
-aumento = 15
-salário_final = salário + (salário * aumento / 100)
-print('O salário do funcionário com aumento de {}% é R${:.2f}'.format(aumento, salário_final))
