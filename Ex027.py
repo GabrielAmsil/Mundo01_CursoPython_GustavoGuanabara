3d6e74c3ba0254e22be6fd367f0d8771201c8184
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:52:41 -0300

    Delete Ex027.py

--- Ex027.py
+++
@@ -1,4 +0,0 @@
-nome = str(input('Digite seu nome completo: ')).strip()
-print(f'Vamos ver seu primeiro e ultimo nome')
-n = nome.split()
-print(f'Seu primeiro nome é {n[0]} e seu ultimo nome é {n[-1]}')
