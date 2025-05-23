bb81fcdc61b6ccc3f1712ea72798585a086daefc
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:51:34 -0300

    Delete Ex020.py

--- Ex020.py
+++
@@ -1,9 +0,0 @@
-from random import shuffle
-A1 = str(input('Primeiro aluno:'))
-A2 = str(input('Segundo aluno:'))
-A3 = str(input('Terceiro aluno:'))
-A4 = str(input('Quarto aluno:'))
-alunos = [A1, A2, A3, A4]
-shuffle(alunos)
-print('A ordem de apresentação será:')
-print(alunos)
