bc156c944916084e34b3cf06f1e2935afc99d33a
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:52:04 -0300

    Delete Ex023.py

--- Ex023.py
+++
@@ -1,9 +0,0 @@
-num = 'Escolha um numero entre 0 e 9999'
-print(num)
-num = int(input('Digite um numero: '))
-print(f'Analisando o numero {num}')
-print(f'Unidade: {num // 1 % 10}')
-print(f'Dezena: {num // 10 % 10}')
-print(f'Centena: {num // 100 % 10}')
-print(f'Milhar: {num // 1000 % 10}')
-print('Fim do progama')
