c163719a1e61fb7089f28f806edc170b77819844
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:53:11 -0300

    Delete Ex031.py

--- Ex031.py
+++
@@ -1,7 +0,0 @@
-d = float(input('Digite a distância da viagem em km: '))
-print('\033[0;33;35mSua viagem de {} vai começar, senta a bunda na cadeira\33[m'.format(d))
-if d <= 200:
-    print(f'O preço da passagem é R${d * 0.50:.2f}')
-else:
-    print(f'O preço da passagem é R${d * 0.45:.2f}')
-        
