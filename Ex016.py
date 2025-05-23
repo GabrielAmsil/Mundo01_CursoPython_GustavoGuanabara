dd5b74fa3f2a7bc78bc6c206868a203606a1c295
Gabriel Amaral Vidal Da Silva <164888394+GabrielAmsil@users.noreply.github.com>
2025-05-23 13:51:10 -0300

    Delete Ex016.py

--- Ex016.py
+++
@@ -1,6 +0,0 @@
-'''from math import trunc
-n = float(input('Digite um numero real: '))
-print('O número digitado foi {} e a sua porção inteira é {}'.format(n, math.trunc(n)))'''
-
-num = float(input('Digite um número real: '))
-print ('O número digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
