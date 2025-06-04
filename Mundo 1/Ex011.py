7e0e628672df543efc2be97a73751485273aba44
Gabriel Amaral Vidal da Silva <gabriel.amsil06@gmail.com>
2025-06-04 14:13:25 -0300

    Exercicío 11

---
+++ Mundo 1/Ex011.py
@@ -0,0 +1,11 @@
+
+#larg = float(input('Largura da parede:'))
+#alt = float(input('Altura da parede:'))
+#print('A área da parede é {} m²'.format(área))
+#tinta = área / 2
+#print('A quantidade de tinta necessária para pintar a parede é {} L'.format(tinta))
+
+Largura = float(input('Largura da parede:'))
+Altura = float(input('Altura da parede:'))
+print('A área da parede é {} m²'.format(Largura * Altura))
+print('A quantidade de tinta necessária para pintar a parede é {} L'.format(Largura * Altura / 2))
